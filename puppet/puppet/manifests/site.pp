4e8beef7f63eb11badb727d065ebc0040fb02050
Mika Lackman <mika.lackman@gmail.com>
2017-05-20 15:24:52 +0300

    python3 virtualenvironment

--- puppet/puppet/manifests/site.pp
+++ puppet/puppet/manifests/site.pp
@@ -1,5 +1,10 @@
 include stdlib
 
+package { "python3-virtualenv":
+  name   => 'python3-venv',
+  ensure => present,
+}
+
 user { "workinnordictwitter":
   ensure     => present,
   shell      => '/bin/bash',
