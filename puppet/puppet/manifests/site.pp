aa75e5303c1b770e76ccb5df37566655e0ec4e02
Mika Lackman <mika.lackman@gmail.com>
2017-05-20 15:40:15 +0300

    python3-pip package

--- puppet/puppet/manifests/site.pp
+++ puppet/puppet/manifests/site.pp
@@ -4,6 +4,15 @@ package { "python3-virtualenv":
   name   => 'python3-venv',
   ensure => present,
 }
+package { "python3-pip":
+  name   => "python3-pip",
+  ensure => present,
+}->
+exec { "upgrade-pip":
+  cwd     => '/',
+  path    => ['/usr/bin'],
+  command => 'sudo pip3 install --upgrade pip'
+}
 
 user { "workinnordictwitter":
   ensure     => present,
