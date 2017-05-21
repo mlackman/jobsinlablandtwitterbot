c79592c797b06ec9b8796e29fc17e33a0da012d2
Mika Lackman <mika.lackman@gmail.com>
2017-05-21 20:41:05 +0300

    feed test

--- rss2tweet/tests/test_feed.py
+++ rss2tweet/tests/test_feed.py
@@ -6,16 +6,22 @@ from rss2tweet.tests import sample_feeds
 
 class TestFeed(unittest.TestCase):
 
+  def setUp(self):
+    self.feed_service_mock = yamf.Mock(feeds.FeedService)
+    self.feed_storage_mock = yamf.Mock(feeds.FeedStorage)
+    self.feed = feeds.Feed("feeder", self.feed_service_mock, self.feed_storage_mock)
 
   def test_when_no_feed_in_storage_all_entries_in_rss_are_new(self):
-    feed_service_mock = yamf.Mock(feeds.FeedService)
-    feed_storage_mock = yamf.Mock(feeds.FeedStorage)
+    self.feed_service_mock.read_feed.returns(sample_feeds.feed1)
+    self.feed_storage_mock.feed_exists.returns(False)
+    self.assertEquals(2, len(list(self.feed.get_new_entries())))
 
-    feed_service_mock.read_feed.returns(sample_feeds.feed1)
-    feed_storage_mock.feed_exists.returns(False)
-    feed = feeds.Feed("feeder", feed_service_mock, feed_storage_mock)
+  def test_when_all_feeds_in_storage_then_no_new_feeds(self):
+    self.feed_storage_mock.feed_exists.returns(True)
+    self.feed_storage_mock.get.returns(sample_feeds.feed1)
+    self.feed_service_mock.read_feed.returns(sample_feeds.feed1)
+    self.assertEquals(0, len(list(self.feed.get_new_entries())))
 
-    self.assertEquals(2, len(list(feed.get_new_entries())))
 
 
 
